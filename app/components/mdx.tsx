1b3d7c61fa48d561ba9446fe33357787fc4128c2
AdithyanI <adithyan.i4internet@gmail.com>
2025-04-10 10:05:14 +0200

    Refactor MDX component for improved readability and consistency
    - Reorganized import statements for better clarity and consistency in the `mdx.tsx` file.
    - Simplified JSX syntax by removing unnecessary semicolons and ensuring consistent use of single quotes.
    - Enhanced the `Code` component to handle empty children gracefully, improving robustness.
    - Updated the `createHeading` function to streamline the creation of heading elements and maintain consistent formatting.
    - Continued focus on comprehensive documentation to support future developers in understanding the code structure and functionality.

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -1,51 +1,49 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { MDXRemote } from "next-mdx-remote/rsc";
-import { highlight } from "sugar-high";
-import { TweetComponent } from "./tweet";
-import { CaptionComponent } from "./caption";
-import { YouTubeComponent } from "./youtube";
-import { ImageGrid } from "./image-grid";
-import rehypeKatex from "rehype-katex";
-import remarkMath from "remark-math";
-import "katex/dist/katex.min.css";
+import 'katex/dist/katex.min.css'
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
+import { highlight } from 'sugar-high'
+import { CaptionComponent } from './caption'
+import { ImageGrid } from './image-grid'
+import { TweetComponent } from './tweet'
+import { YouTubeComponent } from './youtube'
 
 function CustomLink(props) {
-  let href = props.href;
-  if (href.startsWith("/")) {
+  let href = props.href
+  if (href.startsWith('/')) {
     return (
       <Link href={href} {...props}>
         {props.children}
       </Link>
-    );
+    )
   }
-  if (href.startsWith("#")) {
-    return <a {...props} />;
+  if (href.startsWith('#')) {
+    return <a {...props} />
   }
-  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+  return <a target="_blank" rel="noopener noreferrer" {...props} />
 }
 
 function RoundedImage(props) {
-  return <Image alt={props.alt} className="rounded-lg" {...props} />;
+  return <Image alt={props.alt} className="rounded-lg" {...props} />
 }
 
 function Code({ children, ...props }) {
-  let codeHTML = highlight(children);
-  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
+  let codeHTML = children ? highlight(children) : ''
+  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
 }
 
 function Table({ data }) {
-  let headers = data.headers.map((header, index) => (
-    <th key={index}>{header}</th>
-  ));
+  let headers = data.headers.map((header, index) => <th key={index}>{header}</th>)
   let rows = data.rows.map((row, index) => (
     <tr key={index}>
       {row.map((cell, cellIndex) => (
         <td key={cellIndex}>{cell}</td>
       ))}
     </tr>
-  ));
+  ))
   return (
     <table>
       <thead>
@@ -53,11 +51,11 @@ function Table({ data }) {
       </thead>
       <tbody>{rows}</tbody>
     </table>
-  );
+  )
 }
 
 function Strikethrough(props) {
-  return <del {...props} />;
+  return <del {...props} />
 }
 
 function Callout(props) {
@@ -66,7 +64,7 @@ function Callout(props) {
       <div className="flex items-center w-4 mr-4">{props.emoji}</div>
       <div className="w-full callout leading-relaxed">{props.children}</div>
     </div>
-  );
+  )
 }
 
 function slugify(str) {
@@ -74,30 +72,30 @@ function slugify(str) {
     .toString()
     .toLowerCase()
     .trim()
-    .replace(/\s+/g, "-")
-    .replace(/&/g, "-and-")
-    .replace(/[^\w\-]+/g, "")
-    .replace(/\-\-+/g, "-");
+    .replace(/\s+/g, '-')
+    .replace(/&/g, '-and-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
 }
 
 function createHeading(level) {
   const Heading = ({ children }) => {
-    let slug = slugify(children);
+    let slug = slugify(children)
     return React.createElement(
       `h${level}`,
       { id: slug },
       [
-        React.createElement("a", {
+        React.createElement('a', {
           href: `#${slug}`,
           key: `link-${slug}`,
-          className: "anchor",
+          className: 'anchor',
         }),
       ],
       children
-    );
-  };
-  Heading.displayName = `Heading${level}`;
-  return Heading;
+    )
+  }
+  Heading.displayName = `Heading${level}`
+  return Heading
 }
 
 let components = {
@@ -117,7 +115,7 @@ let components = {
   Table,
   del: Strikethrough,
   Callout,
-};
+}
 
 export function CustomMDX(props) {
   return (
@@ -131,5 +129,5 @@ export function CustomMDX(props) {
         },
       }}
     />
-  );
+  )
 }

895864b9fdd801ae8e5b93266fd7abc1af71eb0c
AdithyanI <adithyan.i4internet@gmail.com>
2025-12-03 17:29:01 +0100

    reorder brand stores by popularity (most famous first)

--- data/brands/brand-stores.ts
+++ data/brands/brand-stores.ts
@@ -12,302 +12,67 @@ export type Brand = {
   imageUrl: string;
 };
 
+const CDN_BASE = "https://storage.aipodcast.ing/permanent/brand_stores";
+
+function brand(name: string, slug: string): Brand {
+  return { name, slug, imageUrl: `${CDN_BASE}/${slug}.png` };
+}
+
 /**
  * All 50 food chain brands with AI-generated miniature store illustrations
+ * Ordered by popularity/fame (most famous first)
  */
 export const BRAND_STORES: Brand[] = [
-  {
-    name: "Arby's",
-    slug: "arbys",
-    imageUrl: "https://storage.aipodcast.ing/permanent/brand_stores/arbys.png",
-  },
-  {
-    name: "Au Bon Pain",
-    slug: "au_bon_pain",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/au_bon_pain.png",
-  },
-  {
-    name: "Baskin-Robbins",
-    slug: "baskin_robbins",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/baskin_robbins.png",
-  },
-  {
-    name: "Ben & Jerry's",
-    slug: "ben_and_jerrys",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/ben_and_jerrys.png",
-  },
-  {
-    name: "Blue Bottle Coffee",
-    slug: "blue_bottle_coffee",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/blue_bottle_coffee.png",
-  },
-  {
-    name: "Buffalo Wild Wings",
-    slug: "buffalo_wild_wings",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/buffalo_wild_wings.png",
-  },
-  {
-    name: "Burger King",
-    slug: "burger_king",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/burger_king.png",
-  },
-  {
-    name: "Carl's Jr.",
-    slug: "carls_jr",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/carls_jr.png",
-  },
-  {
-    name: "Chick-fil-A",
-    slug: "chick_fil_a",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/chick_fil_a.png",
-  },
-  {
-    name: "Chipotle",
-    slug: "chipotle",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/chipotle.png",
-  },
-  {
-    name: "Cold Stone",
-    slug: "cold_stone",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/cold_stone.png",
-  },
-  {
-    name: "Costa Coffee",
-    slug: "costa_coffee",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/costa_coffee.png",
-  },
-  {
-    name: "Culver's",
-    slug: "culvers",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/culvers.png",
-  },
-  {
-    name: "Dairy Queen",
-    slug: "dairy_queen",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/dairy_queen.png",
-  },
-  {
-    name: "Del Taco",
-    slug: "del_taco",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/del_taco.png",
-  },
-  {
-    name: "Domino's",
-    slug: "dominos",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/dominos.png",
-  },
-  {
-    name: "Dunkin'",
-    slug: "dunkin",
-    imageUrl: "https://storage.aipodcast.ing/permanent/brand_stores/dunkin.png",
-  },
-  {
-    name: "El Pollo Loco",
-    slug: "el_pollo_loco",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/el_pollo_loco.png",
-  },
-  {
-    name: "Firehouse Subs",
-    slug: "firehouse_subs",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/firehouse_subs.png",
-  },
-  {
-    name: "Five Guys",
-    slug: "five_guys",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/five_guys.png",
-  },
-  {
-    name: "Hardee's",
-    slug: "hardees",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/hardees.png",
-  },
-  {
-    name: "Häagen-Dazs",
-    slug: "haagen_dazs",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/häagen_dazs.png",
-  },
-  {
-    name: "In-N-Out",
-    slug: "in_n_out",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/in_n_out.png",
-  },
-  {
-    name: "Jack in the Box",
-    slug: "jack_in_the_box",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/jack_in_the_box.png",
-  },
-  {
-    name: "Jamba Juice",
-    slug: "jamba_juice",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/jamba_juice.png",
-  },
-  {
-    name: "Jersey Mike's",
-    slug: "jersey_mikes",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/jersey_mikes.png",
-  },
-  {
-    name: "Jimmy John's",
-    slug: "jimmy_johns",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/jimmy_johns.png",
-  },
-  {
-    name: "KFC",
-    slug: "kfc",
-    imageUrl: "https://storage.aipodcast.ing/permanent/brand_stores/kfc.png",
-  },
-  {
-    name: "Krispy Kreme",
-    slug: "krispy_kreme",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/krispy_kreme.png",
-  },
-  {
-    name: "McDonald's",
-    slug: "mcdonalds",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/mcdonalds.png",
-  },
-  {
-    name: "Moe's Southwest Grill",
-    slug: "moes_southwest_grill",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/moes_southwest_grill.png",
-  },
-  {
-    name: "Nando's",
-    slug: "nandos",
-    imageUrl: "https://storage.aipodcast.ing/permanent/brand_stores/nandos.png",
-  },
-  {
-    name: "Panda Express",
-    slug: "panda_express",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/panda_express.png",
-  },
-  {
-    name: "Panera Bread",
-    slug: "panera_bread",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/panera_bread.png",
-  },
-  {
-    name: "Peet's Coffee",
-    slug: "peets_coffee",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/peets_coffee.png",
-  },
-  {
-    name: "Pizza Hut",
-    slug: "pizza_hut",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/pizza_hut.png",
-  },
-  {
-    name: "Popeyes",
-    slug: "popeyes",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/popeyes.png",
-  },
-  {
-    name: "Potbelly",
-    slug: "potbelly",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/potbelly.png",
-  },
-  {
-    name: "Qdoba",
-    slug: "qdoba",
-    imageUrl: "https://storage.aipodcast.ing/permanent/brand_stores/qdoba.png",
-  },
-  {
-    name: "Raising Cane's",
-    slug: "raising_canes",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/raising_canes.png",
-  },
-  {
-    name: "Shake Shack",
-    slug: "shake_shack",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/shake_shack.png",
-  },
-  {
-    name: "Smoothie King",
-    slug: "smoothie_king",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/smoothie_king.png",
-  },
-  {
-    name: "Sonic",
-    slug: "sonic",
-    imageUrl: "https://storage.aipodcast.ing/permanent/brand_stores/sonic.png",
-  },
-  {
-    name: "Starbucks",
-    slug: "starbucks",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/starbucks.png",
-  },
-  {
-    name: "Subway",
-    slug: "subway",
-    imageUrl: "https://storage.aipodcast.ing/permanent/brand_stores/subway.png",
-  },
-  {
-    name: "Taco Bell",
-    slug: "taco_bell",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/taco_bell.png",
-  },
-  {
-    name: "Tim Hortons",
-    slug: "tim_hortons",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/tim_hortons.png",
-  },
-  {
-    name: "Wendy's",
-    slug: "wendys",
-    imageUrl: "https://storage.aipodcast.ing/permanent/brand_stores/wendys.png",
-  },
-  {
-    name: "Whataburger",
-    slug: "whataburger",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/whataburger.png",
-  },
-  {
-    name: "Wingstop",
-    slug: "wingstop",
-    imageUrl:
-      "https://storage.aipodcast.ing/permanent/brand_stores/wingstop.png",
-  },
+  brand("Starbucks", "starbucks"),
+  brand("McDonald's", "mcdonalds"),
+  brand("KFC", "kfc"),
+  brand("Dunkin'", "dunkin"),
+  brand("Krispy Kreme", "krispy_kreme"),
+  brand("Taco Bell", "taco_bell"),
+  brand("Subway", "subway"),
+  brand("Pizza Hut", "pizza_hut"),
+  brand("Domino's", "dominos"),
+  brand("Chick-fil-A", "chick_fil_a"),
+  brand("Wendy's", "wendys"),
+  brand("Burger King", "burger_king"),
+  brand("Popeyes", "popeyes"),
+  brand("Panda Express", "panda_express"),
+  brand("Chipotle", "chipotle"),
+  brand("Five Guys", "five_guys"),
+  brand("In-N-Out", "in_n_out"),
+  brand("Shake Shack", "shake_shack"),
+  brand("Dairy Queen", "dairy_queen"),
+  brand("Baskin-Robbins", "baskin_robbins"),
+  brand("Cold Stone", "cold_stone"),
+  brand("Ben & Jerry's", "ben_and_jerrys"),
+  brand("Häagen-Dazs", "häagen_dazs"),
+  brand("Tim Hortons", "tim_hortons"),
+  brand("Costa Coffee", "costa_coffee"),
+  brand("Peet's Coffee", "peets_coffee"),
+  brand("Blue Bottle Coffee", "blue_bottle_coffee"),
+  brand("Panera Bread", "panera_bread"),
+  brand("Au Bon Pain", "au_bon_pain"),
+  brand("Jamba Juice", "jamba_juice"),
+  brand("Smoothie King", "smoothie_king"),
+  brand("Wingstop", "wingstop"),
+  brand("Buffalo Wild Wings", "buffalo_wild_wings"),
+  brand("Nando's", "nandos"),
+  brand("Raising Cane's", "raising_canes"),
+  brand("Sonic", "sonic"),
+  brand("Arby's", "arbys"),
+  brand("Jack in the Box", "jack_in_the_box"),
+  brand("Carl's Jr.", "carls_jr"),
+  brand("Hardee's", "hardees"),
+  brand("Whataburger", "whataburger"),
+  brand("Culver's", "culvers"),
+  brand("Potbelly", "potbelly"),
+  brand("Jimmy John's", "jimmy_johns"),
+  brand("Jersey Mike's", "jersey_mikes"),
+  brand("Firehouse Subs", "firehouse_subs"),
+  brand("Qdoba", "qdoba"),
+  brand("Moe's Southwest Grill", "moes_southwest_grill"),
+  brand("Del Taco", "del_taco"),
+  brand("El Pollo Loco", "el_pollo_loco"),
 ];
 
 export const BRAND_COUNT = BRAND_STORES.length;
